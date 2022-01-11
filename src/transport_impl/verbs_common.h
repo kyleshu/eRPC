367d6f45014186356f12ec1dbd5ea2b32b62ef45
Kyle Shu <shujunyi@gmail.com>
2022-01-12 00:01:18 +0800

    try to fix bug

--- src/transport_impl/verbs_common.h
+++ src/transport_impl/verbs_common.h
@@ -59,8 +59,8 @@ static Transport::mem_reg_info ibv_reg_mr_wrapper(struct ibv_pd *pd, void *buf,
   struct ibv_mr *mr = ibv_reg_mr(pd, buf, size, IBV_ACCESS_LOCAL_WRITE);
   rt_assert(mr != nullptr, "Failed to register mr.");
 
-  char* end = static_cast<char*>(buf) + size;
-  printf("registered memory region: %p - %p\n", static_cast<void*>(buf), static_cast<void*>(end));
+  //char* end = static_cast<char*>(buf) + size;
+  //printf("registered memory region: %p - %p\n", static_cast<void*>(buf), static_cast<void*>(end));
 
   ERPC_INFO("Registered %zu MB (lkey = %u)\n", size / MB(1), mr->lkey);
   return Transport::mem_reg_info(mr, mr->lkey);
